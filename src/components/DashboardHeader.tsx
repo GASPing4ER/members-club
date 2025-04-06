00832c645d8f2f1eb81f6a18cf1e18112012d4c5
GASPing4ER <simecgasper@gmail.com>
2025-04-06 14:10:04 +0200

    mobile header optimization

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -6,15 +6,23 @@ import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { useState } from "react";
 import { AddEventDialog } from "@/components";
+import { Menu, X } from "lucide-react";
 
 const DashboardHeader = () => {
   const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <>
       <header className="fixed top-0 left-0 w-full h-20 flex justify-center items-center gap-8 border-b bg-white z-50">
-        <ul className="flex gap-8">
+        <button
+          className="md:hidden"
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+        >
+          {mobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+        <ul className="hidden md:flex gap-8">
           {navItems.slice(0, 2).map((item) => (
             <li key={item.path}>
               <Link
@@ -40,7 +48,7 @@ const DashboardHeader = () => {
         </Link>
 
         {/* Right side navigation */}
-        <ul className="flex gap-8">
+        <ul className="hidden md:flex gap-8">
           {navItems.slice(2).map((item) => (
             <li key={item.path}>
               <Link
@@ -59,9 +67,36 @@ const DashboardHeader = () => {
             </li>
           ))}
         </ul>
-        <Button onClick={() => setIsOpen(true)} className="absolute right-10">
+        <Button
+          onClick={() => setIsOpen(true)}
+          className="md:absolute md:right-10"
+        >
           Create Event
         </Button>
+        {mobileMenuOpen && (
+          <div className="fixed inset-0 top-20 bg-white z-40 p-4 md:hidden">
+            <ul className="flex flex-col gap-4">
+              {navItems.map((item) => (
+                <li key={item.path}>
+                  <Link
+                    href={item.path}
+                    className={`relative py-2 px-4 block transition-all duration-300 ${
+                      pathname === item.path
+                        ? "text-[#D5BDA4]"
+                        : "hover:text-[#D5BDA4]"
+                    }`}
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    {item.name}
+                    {pathname === item.path && (
+                      <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#D5BDA4] rounded-full" />
+                    )}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </header>
       <AddEventDialog isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
