ad236d639e81caefaafa63276c2ec6eb86dc3434
GASPing4ER <simecgasper@gmail.com>
2025-04-06 13:59:54 +0200

    event details page

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -14,6 +14,10 @@ export type EventProps = {
 
 const EventsPage = async () => {
   const { data: events } = await getEvents();
+
+  if (!events || events.length === 0) {
+    return <main className="p-10">No events found.</main>;
+  }
   return (
     <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
       {events &&
